37d678ce042c0c6df225bea4271cdc7ea97ec309
Joost Plattel <jsplattel@gmail.com>
2023-05-02 15:31:28 +0200

    rm pprint

--- erpnext_foxycart_integration/api.py
+++ erpnext_foxycart_integration/api.py
@@ -145,11 +145,6 @@ def make_sales_order(customer, address, foxycart_data, foxycart_settings):
 
 	# sales_order.base_grand_total = 0
 
-	import pprint from pprint
-
-	pp = pprint.PrettyPrinter(indent=4)
-	pp.pprint(sales_order)
-
 	sales_order.customer_address = address
 	sales_order.shipping_address_name = address
 	
